da25262ce2ef44f7c35101f7e4bb5c370cd23fb8
ho3eingh <hossein.gholizadeh.2001@gmail.com>
2023-12-06 01:25:39 +0330

    feat (pe-parser) added ReadDosHeader func

--- main.cpp
+++ main.cpp
@@ -4,9 +4,41 @@
 
 
 using namespace std;
+// Changes the default struct packing alignment to 1 byte
+#pragma pack(push, 1)
 
+struct IMAGE_DOS_HEADER {
+    uint16_t e_magic;
+    int32_t e_lfanew;
+};
+
+// Restores the default struct packing alignment.
+#pragma pack(pop)
+
+void readDosHeader(ifstream& file) {
+    IMAGE_DOS_HEADER dosHeader;
+    file.read(reinterpret_cast<char*>(&dosHeader), sizeof(IMAGE_DOS_HEADER));
+
+    cout << "DOS Header:" << hex << uppercase
+         << "\n  Magic Number: " << dosHeader.e_magic
+         << "\n  Address of New Exe Header: " << dosHeader.e_lfanew
+         << endl;
+}
 
 int main() {
+    const char* filePath = "G:\\test.exe";
+    // Opens the file in binary mode
+    ifstream file(filePath, ios::binary);
+
+    if (!file.is_open()) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    // Read and print DOS Header
+    readDosHeader(file);
+
+    file.close();
 
     return 0;
 }
